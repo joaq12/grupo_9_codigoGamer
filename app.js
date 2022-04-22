c9c85ac0334deed3a0b6beadf1812796d2804ae0
MarcosL <marcos.mglnet@gmail.com>
2022-04-21 23:31:16 -0300

    EJS

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const mainRouter = require('./routes/mainRouter')
 app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
 
 app.listen(3030, ()=>
 console.log("Servidor Corriendo"));
 
-app.get("/", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/index.html"))});
-    
-app.get("/login", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/login.html"))});
-    
-app.get("/product-cart", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/product-cart.html"))});
-    
-app.get("/product-detail", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/product-detail.html"))});
-        
-app.get("/register", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/register.html"))});
+app.use('/', mainRouter);
 
-app.get("/store", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/store.html"))});
+// app.use("/login", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/login"))});
 
-app.get("/contact", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/contact.html"))});
-    
-app.get("/checkout-info", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/checkout-info.html"))});
+// app.get("/product-cart", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/product-cart"))});
 
-app.get("/checkout", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/checkout.html"))});
+// app.get("/product-detail", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/product-detail"))});
 
-app.get("/shipping", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/shipping.html"))});
-    
-app.get("/payment", (req,res)=>{
-    res.sendFile(path.resolve(__dirname, "views/payment.html"))});
+// app.get("/register", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/register"))});
+
+// app.get("/store", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/store"))});
+
+// app.get("/contact", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/contact"))});
+
+// app.get("/cart", (req,res)=>{
+//     res.sendFile(path.resolve(__dirname, "views/cart"))});
+
+
