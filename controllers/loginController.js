5c705017a1fcce0e82d3d38247baf90c2ee17660
SacconeR88 <sacconerodrigoe@gmail.com>
2022-04-27 20:35:07 -0300

    paginas nuevas - css - otros

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,7 +1,11 @@
 const loginController={
-     login:(req,res)=>{
+    login:(req,res)=>{
          res.render('login')
      },
+
+    register :(req,res)=>{
+        res.render('register')
+    },
 };
 
 module.exports=loginController;
--- controllers/productosController.js
+++ controllers/productosController.js
@@ -4,8 +4,31 @@ const productosController ={
     },
     cart:(req,res)=>{
         res.render('cart')
-    } 
-}
+    },
+     
+    checkoutAdress:(req,res)=>{
+        res.render('checkout-adress')
+    },
 
+    checkoutConfirm:(req,res)=>{
+        res.render('checkout-confirm')
+    },
+
+    checkoutData:(req,res)=>{
+        res.render('checkout-data')
+    },
+
+    checkoutPayment:(req,res)=>{
+        res.render('checkout-payment')
+    },
+
+    checkoutShipping:(req,res)=>{
+        res.render('Checkout-shipping')
+    },
+
+    productUpload:(req,res)=>{
+        res.render('product-upload-edit')
+    },
+}
 
 module.exports = productosController;
