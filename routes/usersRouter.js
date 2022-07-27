f7d07bcfbf166aa9a67f83ed0b5fb30b72a8f250
amg201512 <amg201512@gmail.com>
2022-07-27 18:55:55 -0500

    agregando apis

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -4,6 +4,8 @@ const router= express.Router();
 
 //controllers
 const usersController= require('../controllers/usersController');
+//api controller
+const apiUserController=require('../api/usersController');
 //middleWares
 const validateRegister=require('../middlewares/validateRegisterMiddleware')
 const validateLogin = require("../middlewares/validateLoginMiddleware")
@@ -13,22 +15,26 @@ const authMiddleware = require("../middlewares/authMiddleware")
 const validateUserEdit = require("../middlewares/validateUserEditMiddleware")
 
 // todos los usuarios
-// router.get('/users',authMiddleware, usersController.allUsers);
+/router.get('/users', usersController.allUsers);
 // perfil de usuario
-// router.get('/user-profile/:id',authMiddleware, usersController.userDetails);
+ router.get('/user-profile/:id', usersController.userDetails);
 // login usuario
-// router.get('/user-login', ghestMiddleware, usersController.login);
-// router.post('/user-login', validateLogin, usersController.loginProcess);
-// router.post("/user-logout",authMiddleware, usersController.logout)
-// registro de usuario
-// router.get('/user-register', ghestMiddleware, usersController.register);
-// router.post('/user-register', ghestMiddleware, uploadFile.single('profilePhoto'),validateRegister,usersController.registerProcess);
-// edicion de usuarios
-// router.get('/user-edit/:id',usersController.userEdit);
-// router.delete('/user-edit/:id',usersController.userDelete);
-// router.put('/user-edit/:id',uploadFile.single('profilePhoto'),validateUserEdit,usersController.userUpdate);
+router.get('/user-login', usersController.login);
+router.post('/user-login', validateLogin, usersController.loginProcess);
+router.post("/user-logout", usersController.logout)
+//registro de usuario
+router.get('/user-register', usersController.register);
+router.post('/user-register', uploadFile.single('profilePhoto'),validateRegister,usersController.registerProcess);
 
-// router.get("/ghestUser", usersController.ghest)
-// router.get("/authUser", usersController.auth)
+////edicion de usuarios
+router.get('/user-edit/:id',usersController.userEdit);
+router.put('/user-edit/:id',uploadFile.single('profilePhoto'),usersController.userUpdate)
+router.delete('/user-edit/:id',usersController.userDelete);
+router.get("/ghestUser", usersController.ghest)
+router.get("/authUser", usersController.auth)
+
+//Rutas API
+router.get('/api/users',apiUserController.all);
+router.get('/api/users/:id',apiUserController.showUser);
 
 module.exports= router;
